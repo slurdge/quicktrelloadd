37893cc60031a30e684810d463e6d65380ab1c51
slurdge <slurdge@slurdge.org>
2016-06-02 15:27:10 +0900

    Update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QuickTrelloAdd")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © slurdge 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
